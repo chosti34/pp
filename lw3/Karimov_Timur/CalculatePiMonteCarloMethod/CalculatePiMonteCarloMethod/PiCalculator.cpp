f2607b2090fe5361c5bba4f78c0c56c342bb6df2
Timur <chosti34@gmail.com>
2018-05-22 10:19:19 +0300

    Добавил константу и изменил кодировку

--- lw3/Karimov_Timur/CalculatePiMonteCarloMethod/CalculatePiMonteCarloMethod/PiCalculator.cpp
+++ lw3/Karimov_Timur/CalculatePiMonteCarloMethod/CalculatePiMonteCarloMethod/PiCalculator.cpp
@@ -1,15 +1,17 @@
-#include "stdafx.h"
+п»ї#include "stdafx.h"
 #include "PiCalculator.h"
 #include "Random.h"
 #include <omp.h>
 
 namespace
 {
-// Подразумевается, что центр окружности имеет координаты (0, 0)
+// РџРѕРґСЂР°Р·СѓРјРµРІР°РµС‚СЃСЏ, С‡С‚Рѕ С†РµРЅС‚СЂ РѕРєСЂСѓР¶РЅРѕСЃС‚Рё РёРјРµРµС‚ РєРѕРѕСЂРґРёРЅР°С‚С‹ (0, 0)
 bool CircleHitTest(float x, float y, float radius)
 {
 	return std::pow(x, 2) + std::pow(y, 2) <= std::pow(radius, 2);
 }
+
+const float RADIUS = 1.f;
 }
 
 float PiCalculator::Calculate(int iterations)
@@ -22,9 +24,9 @@ float PiCalculator::Calculate(int iterations)
 		#pragma omp for
 		for (int i = 0; i < iterations; ++i)
 		{
-			const float x = random.Get(-1.f, 1.f);
-			const float y = random.Get(-1.f, 1.f);
-			if (CircleHitTest(x, y, 1.f))
+			const float x = random.Get(-RADIUS, RADIUS);
+			const float y = random.Get(-RADIUS, RADIUS);
+			if (CircleHitTest(x, y, RADIUS))
 			{
 				#pragma omp atomic
 				++inside;
